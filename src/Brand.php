88419ce69cd3fdff6cb8d718a8b8fae0270b7eeb
Minh Phuong <mphuong@kent.edu>
2017-05-26 14:57:25 -0700

    add field and constructor

---
+++ src/Brand.php
@@ -0,0 +1,20 @@
+<?php
+    class Brand
+    {
+      //property
+      private $Name;
+      private $id;
+
+      //constructor
+      function __construct($Name, $id = null)
+      {
+        $this->Name = $Name;
+        $this->id = $id;
+      }
+
+    
+
+
+
+      }
+?>
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -12,5 +12,31 @@ $username = 'root';
 $password = 'root';
 $DB = new PDO($server,$username,$password);
 
+//Same name as file
+  class BrandTest extends PHPUnit_Framework_TestCase
+  {
 
+    function test_getAll()
+    {
+      //Arrange
+      $name1 = "Versace";
+      $test_brand1 = new Brand($name1);
+      $test_brand1->save();
+
+      $name2 = "Photos";
+      $test_brand2 = new Brand($name2);
+      $test_brand2->save();
+      //Act
+      $expected = array($test_brand1, $test_brand2);
+      $result = Brand::getAll();
+      //Assert
+      $this->assertEquals($expected, $result);
+    }
+
+    protected function tearDown()
+    {
+      Brand::deleteAll();
+      Store::deleteAll();
+    }
+  }
  ?>
