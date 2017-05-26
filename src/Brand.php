74802f6d1e13b63127b0a692a170cdc70c285bc8
Minh Phuong <mphuong@kent.edu>
2017-05-26 14:59:51 -0700

    add get all method

--- src/Brand.php
+++ src/Brand.php
@@ -27,6 +27,18 @@
         return $this->id;
       }
 
+      static function getAll()
+      {
+        $db_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+        $brands=array();
+        foreach ($db_brands as $brand) {
+          $new_name=$brand['name'];
+          $id=$brand['id'];
+          $new_brand= new Brand($new_Name,$id);
+          array_push($brands,$new_brand);
+        }
+        return $brands;
+      }
 
 
 
